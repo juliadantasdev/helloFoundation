c9d2a1658ab3dbcac75b6453ac9373945b80a22d
Júlia Dantas <julia.dantas62@aluno.ifce.edu.br>
2023-02-17 17:25:21 -0300

    Initial Commit

---
+++ helloFoundation/helloFoundationApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloFoundationApp.swift
+//  helloFoundation
+//
+//  Created by user on 17/02/23.
+//
+
+import SwiftUI
+
+@main
+struct helloFoundationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
